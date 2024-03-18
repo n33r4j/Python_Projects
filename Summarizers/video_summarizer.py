f7792ede718fb58fd4598ba8dbe14ae0b3902157
n33r4j <52103565+n33r4j@users.noreply.github.com>
2024-03-19 04:44:08 +1100

    Add --force and --trunc and token counter

--- Summarizers/video_summarizer.py
+++ Summarizers/video_summarizer.py
@@ -4,13 +4,25 @@
 # This only works for videos with a single narrator. For example TED talks.
 # For videos with multiple people speaking, try to convert the xml into a usable script. TODO
 
+# dotenv reference (https://saurabh-kumar.com/python-dotenv/)
+
 from pytube import YouTube
 import re
 import argparse
 from openai import OpenAI
 from dotenv import load_dotenv
 import pyttsx3
+import tiktoken
 
+
+MODEL = "gpt-3.5-turbo"
+MAX_LIMIT = 3800 # Actual limit is 4096 tokens (it's actually 16385 tokens ?)
+
+
+def num_tokens_from_string(in_string: str, encoding_name: str) -> int:
+	encoding = tiktoken.encoding_for_model(encoding_name)
+	num_tokens = len(encoding.encode(in_string))
+	return num_tokens
 
 
 def getTranscriptAsText(link):
@@ -51,14 +63,13 @@ def getCleanedText(text):
 
 def breakIntoPrompts(words, print_prompts=False):
 	# To feed ChatGPT
-	LIMIT = 2950 # Actual limit is 3000 words
 	queries = []
 	words = words.split()
 	
 	while words:
-		if len(words) > LIMIT:
-			queries.append(" ".join(words[:LIMIT]))
-			words = words[LIMIT:]
+		if len(words) > MAX_LIMIT:
+			queries.append(" ".join(words[:MAX_LIMIT]))
+			words = words[MAX_LIMIT:]
 		else: 
 			queries.append(" ".join(words[:-1]))
 			words = []
@@ -87,22 +98,32 @@ def speakText(prose):
 	engine.say(prose)
 	engine.runAndWait()
 
-def getSummary(queries, speak=False):
-	if len(queries) > 1:
+def getSummary(queries, speak=False, force=False, trunc=False):
+	if len(queries) > 1 and not force and not trunc:
 		print(len(queries))
 		for q in queries:
-			print(q, len(q))
+			print(q)
+			print(len(q))
 		
-		print("Since ChatGPT only takes prompts upto 3000 words, we have to break up this transcript into several parts. This functionality is yet to be implemented here.")
+		print("ChatGPT 3.5 turbo can only accept 4096 tokens. So we either need to trim down this text or use ChatGPT 4.0, which can take up to 20.000 tokens I think.")
+		print("If you'd like to try submitting the prompt anyway, try again using the '-f' flag.")
+
 	else:
 		isAPIKeyLoaded = load_dotenv()
 		client = OpenAI()
 		
+		if force:
+			token_count = num_tokens_from_string(" ".join(queries), MODEL)
+			print("token count: ", token_count)
+			if token_count > MAX_LIMIT:
+				print(f"The number of tokens is greater than the soft limit of {MAX_LIMIT} tokens. Because of this, there's a chance that the response might be incomplete.")
+				print("Submitting anyway...")
+		
 		completion = client.chat.completions.create(
-			model="gpt-3.5-turbo",
+			model=MODEL,
 			messages=[
 				{"role": "system", "content": "You are an expert in STEM and summarizing a wide variety of topics."},
-				{"role": "user", "content": "Summarize the following text in up to 5 bullet points. Also list the key takeaways." + queries[0]}
+				{"role": "user", "content": "Summarize the following text in up to 5 bullet points. Also list the key takeaways." + (" ".join(queries))}
 			]
 		)
 
@@ -120,15 +141,25 @@ if __name__ == "__main__":
 	
 	parser.add_argument("-l", "--link", help = "Link to the YouTube video.", required = True)
 	parser.add_argument("-s", "--skip", help = "Skip the summarization", action="store_true")
+	parser.add_argument("-f", "--force", help = "Try submitting the query to ChatGPT anyway and hope for the best", action="store_true")
+	parser.add_argument("-t", "--trunc", help = "TODO: Submit the first 3000 words as a query to ChatGPT", action="store_true")
 	
 	args = parser.parse_args()
 	
-	transcript = getTranscriptAsText(args.link)
-	clean_text = getCleanedText(transcript)
+	# TODO
+	if args.trunc:
+		print("Sorry, --trunc hasn't been implemented yet.")
 	
-	if not args.skip:
-		prompts = breakIntoPrompts(clean_text)
-		getSummary(prompts, speak=True)
+	if args.force and args.trunc:
+		print("Both --force and --trunc can't be set at the same time. Please pick one or neither.")
+	
 	else:
-		print("=== Transcript ===")
-		print(clean_text)
+		transcript = getTranscriptAsText(args.link)
+		clean_text = getCleanedText(transcript)
+		
+		if not args.skip:
+			prompts = breakIntoPrompts(clean_text)
+			getSummary(prompts, speak=True, force=args.force, trunc=args.trunc)
+		else:
+			print("=== Transcript ===")
+			print(clean_text)
